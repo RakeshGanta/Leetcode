44e625d5b292a9d6abac8a4cf0506652c8c65df8
Rakesh <rakeshganta98@gmail.com>
2024-02-15 21:06:17 +0530

    Time: 62 ms (24.81%) | Memory: 51.3 MB (5.22%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.js
@@ -0,0 +1,32 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, next, random) {
+ *    this.val = val;
+ *    this.next = next;
+ *    this.random = random;
+ * };
+ */
+
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+var copyRandomList = function(head) {
+    const res=new ListNode();
+    let temp=res;
+    const map=new Map();
+    let input=head;
+    let input2=head;
+    while(input!=null){
+        map.set(input,new ListNode(input.val));
+        input=input.next;
+    }
+    while(input2!==null){
+        temp.next=map.get(input2);
+        temp.next.random=map.get(input2.random);
+        temp=temp.next;
+        input2=input2.next;
+    }
+    console.log(res);
+    return res.next;
+};
