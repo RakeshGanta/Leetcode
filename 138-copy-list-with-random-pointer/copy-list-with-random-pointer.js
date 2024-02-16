b888f595bf5de1c1d1e98d4c5fb94ac8c00bbe15
Rakesh <rakeshganta98@gmail.com>
2024-02-16 09:36:15 +0530

    Time: 56 ms (49.50%) | Memory: 51.5 MB (5.67%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.js
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.js
@@ -14,18 +14,44 @@
 var copyRandomList = function(head) {
     const res=new ListNode();
     let temp=res;
-    const map=new Map();
+    // const map=new Map();
     let input=head;
+    // let input2=head;
+    // while(input!=null){
+    //     map.set(input,new ListNode(input.val));
+    //     input=input.next;
+    // }
+    // while(input2!==null){
+    //     temp.next=map.get(input2);
+    //     temp.next.random=map.get(input2.random);
+    //     temp=temp.next;
+    //     input2=input2.next;
+    // }
+    // return res.next;
+    let dummy=input?.next||null
+    while(input!==null){
+        // dummy.next=input.next;
+        // let dummy=input.next;
+        input.next=new ListNode(input.val);
+        input.next.next=dummy;
+        input=dummy;
+        if(dummy!==null){
+            dummy=dummy.next;
+        }
+    }
     let input2=head;
-    while(input!=null){
-        map.set(input,new ListNode(input.val));
-        input=input.next;
-    }
     while(input2!==null){
-        temp.next=map.get(input2);
-        temp.next.random=map.get(input2.random);
-        temp=temp.next;
-        input2=input2.next;
+        input2.next.random=input2.random?.next || null;
+        input2=input2.next.next;
     }
+    let input3=head;
+    // temp.next=input3.next;
+    while(input3!==null){
+        temp.next=input3.next;
+        input3.next=input3.next.next;
+        temp=temp.next;
+        input3=input3.next;
+    }
+    console.log(res.next);
     return res.next;
 };
